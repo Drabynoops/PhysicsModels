effeaec1c36f49a2af831ca0473615b3d9652622
Keenan Barber <barberk0438@uwstout.edu>
2018-02-16 14:37:41 -0600

    Renamed the game to Game and added asteroid class

--- Asteroids/asteroids.py
+++ Asteroids/asteroids.py
@@ -1,12 +1,50 @@
 import pygame
 from pygame import init, quit
+import math
+import random
 
 from color import Color
 from coords import Coords
 from vec2d import Vec2d
 
+ASTEROID_MIN_RADIUS = 20
+ASTEROID_MAX_RADIUS = 30
+ASTEROID_VERTEX_COUNT_MIN = 7
+ASTEROID_VERTEX_COUNT_MAX = 14
+ASTEROID_RADIUS_NOISE = 10
 
-class Asteroids:
+BACKGROUND_COLOR = BLACK
+LINE_COLOR = WHITE
+LINE_THICKNESS = 2
+
+
+class Asteroid:
+    # Constructor
+    def __init__(self, x = 0, y = 0):
+        self.position = Vec2d(x, y)
+        self.vertices = []#[(50, 50), (70, 50), (70, 70)]
+
+        self.asteroidRadius = random.randint(ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS) 
+        
+        # Generate the vertices for the asteroid
+        angleStep = 360 / random.randint(ASTEROID_VERTEX_COUNT_MIN, ASTEROID_VERTEX_COUNT_MAX)
+        currentStep = 0
+        while currentStep <= (360 - angleStep):
+            rad = currentStep * (math.pi / 180)
+            noise = random.randint(-1 * ASTEROID_RADIUS_NOISE, ASTEROID_RADIUS_NOISE) # Adds noise to the shape
+            newVertex = [
+                ((self.asteroidRadius + noise) * math.cos(rad)) + self.position.x, 
+                ((self.asteroidRadius + noise) * math.sin(rad)) + self.position.y
+            ]
+
+            self.vertices.append(newVertex)
+            currentStep += angleStep
+
+    def draw(self, screen):
+        pygame.draw.lines(screen, LINE_COLOR, True, self.vertices, LINE_THICKNESS)
+        
+
+class Game:
 
     def __init__(self, width, height):
         pygame.init()
@@ -29,9 +67,3 @@ if __name__ == "__main__":
     except Exception as e:
         pygame.quit()
         raise 
-elif __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        pygame.quit()
-        raise e
