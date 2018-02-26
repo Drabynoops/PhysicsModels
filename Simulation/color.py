a73e2d65ef58fec4cb2eb1664108cc21d720e249
Keenan Barber <barberk0438@uwstout.edu>
2018-02-26 14:59:07 -0600

    Added vec2d and color fiels

---
+++ Simulation/color.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Color:
+    BLACK    = (   0,   0,   0)
+    WHITE    = ( 255, 255, 255)
+    GREEN    = (   0, 255,   0)
+    RED      = ( 255,   0,   0)
+    BLUE     = (   0,   0, 255)
+    GRAY     = ( 127, 127, 127)
+    GRAY_2   = (  40,  40,  40)
