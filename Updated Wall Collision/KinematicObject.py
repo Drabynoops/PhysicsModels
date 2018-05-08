6322fbf64db177a25d875d4a3b28bf6d270a11e4
Keenan Barber <barberk0438@uwstout.edu>
2018-05-08 17:14:49 -0500

    Removed angvel from KinematicObject constructor to keep us from accidentally putting something for that (causes issues).

--- Updated Wall Collision/KinematicObject.py
+++ Updated Wall Collision/KinematicObject.py
@@ -1,8 +1,8 @@
 from CollisionObject import CollisionObject
 
 class KinematicObject(CollisionObject):
-  def __init__(self, pos, vel, density, points, color, e=0, mu=0, angle=0, angvel=0):
-    super().__init__(pos, vel, density, points, color, e, mu, angle, angvel)
+  def __init__(self, pos, vel, density, points, color, e=0, mu=0, angle=0):
+    super().__init__(pos, vel, density, points, color, e, mu, angle, 0)
     self.type = "kinematic"
     self.moment = 1e99
   
